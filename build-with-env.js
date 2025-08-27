59135e41d6bcc197e20ed950b9e4b68679e5c55c
Dave <bevilledavid@gmail.com>
2025-08-27 21:25:44 +0200

    test5

--- build-with-env.js
+++ build-with-env.js
@@ -5,25 +5,10 @@ import fs from 'fs'
 // Debug: afficher les variables disponibles
 console.log('Variables disponibles:', Object.keys(process.env))
 
-// Transférer les variables Railway
-if (process.env.RAILWAY_PAYLOAD_SECRET) {
-  process.env.PAYLOAD_SECRET = process.env.RAILWAY_PAYLOAD_SECRET
-  console.log('✓ PAYLOAD_SECRET injecté')
-}
-
-if (process.env.RAILWAY_DATABASE_URI) {
-  process.env.DATABASE_URI = process.env.RAILWAY_DATABASE_URI
-  console.log('✓ DATABASE_URI injecté')
-}
-
-if (process.env.RAILWAY_NEXT_PUBLIC_SERVER_URL) {
-  process.env.NEXT_PUBLIC_SERVER_URL = process.env.RAILWAY_NEXT_PUBLIC_SERVER_URL
-  console.log('✓ NEXT_PUBLIC_SERVER_URL injecté')
-}
 
 // Vérification finale
 console.log('PAYLOAD_SECRET présent: ' + process.env.PAYLOAD_SECRET, !!process.env.PAYLOAD_SECRET)
 console.log('DATABASE_URI présent: ' + process.env.DATABASE_URI, !!process.env.DATABASE_URI)
-
+console.log('RAILWAY_NEXT_PUBLIC_SERVER_URL présent: ' + process.env.RAILWAY_NEXT_PUBLIC_SERVER_URL, !!process.env.RAILWAY_NEXT_PUBLIC_SERVER_URL)
 // Lancer le build
 execSync('next build', { stdio: 'inherit' })
